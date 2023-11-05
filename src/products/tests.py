db42cd6aa5c86356bdfd869551fb812e7cccc25a
realsanjeev <realsajeev2@gmail.com>
2023-11-05 20:06:01 +0545

    adding testcase

--- src/products/tests.py
+++ src/products/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Product
+
+class ProductsModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Product.objects.create(title='shirt', price=32.32535)
+
+    def test_title_label(self):
+        product = Product.objects.get(id=1)
+        field_label = product._meta.get_field('title').verbose_name
+        self.assertEqual(field_label, 'title')
+
+    def test_price_has_two_decimal(self):
+        product = Product.objects.get(id=1)
+        decimal_places = product._meta.get_field('price').decimal_places
+        self.assertEqual(decimal_places, 2)
+
+    def test_get_absolute_url(self):
+        product = Product.objects.get(id=1)
+        self.assertEqual(product.get_absolute_url(), '/product/shirt')
